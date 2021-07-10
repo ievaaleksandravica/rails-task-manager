21da1e9206d05833273e2aac0b5c01fb21e2eed5
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-07-10 13:03:26 +0200

    create tasks method done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,22 @@ class TasksController < ApplicationController
     @id = params[:id]
     @task = Task.find(@id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @params = params[:task]
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
